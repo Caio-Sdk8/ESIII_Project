0246c25e8384c5e1337d1de7a28282ec0b39abe5
Igor Fernandes <54910774+1rg0@users.noreply.github.com>
2025-06-01 12:07:48 -0300

    feat: adicionar modal de edição de cadastro e endereços
    Alterações:
    adição de campos obrigatórios;
    adição de modal de edição de cadastro;
    adição de modal de edição de endereços;
    adição de preferência de cartões;
    erro de renderização de ícones resolvido;
    adição de modal de sucesso, erro e exclusão;
    adição de ícones de editar endereço e preferência de cartão.

--- ESIII_ClienTela/obj/Debug/net8.0/ESIII_ClienTela.AssemblyInfo.cs
+++ ESIII_ClienTela/obj/Debug/net8.0/ESIII_ClienTela.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ESIII_ClienTela")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+553a95bacfd0fa1808160cdcdf88c6176cb24747")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea3bb38b7d159ec9277ff2d93be149aad7653f0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ESIII_ClienTela")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ESIII_ClienTela")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
